a24ef729efd30b06f4d4a1bd6fd0b5e35828b3a2
Quentin Gendre <swynfel@gmail.com>
2020-10-03 17:23:57 +0900

    Add wanted poster selection

--- Scripts/GUI/Panels/HuntPanel.cs
+++ Scripts/GUI/Panels/HuntPanel.cs
@@ -1,5 +1,25 @@
 using System;
+using System.Collections.Generic;
 using Godot;
+using Utils;
 
 public class HuntPanel : MarginContainer {
+    [Export] NodePath containerPath;
+    Control containerField;
+
+    public override void _Ready () {
+        containerField = GetNode<Control>(containerPath);
+        Load(new List<Demon> {
+            new Demon(), new Demon(), new Demon(), new Demon(), new Demon()
+        });
+    }
+
+    public void Load (List<Demon> demons) {
+        containerField.QueueFreeChildren();
+        foreach (Demon demon in demons) {
+            WantedTable wanted = WantedTable.Instance();
+            containerField.AddChild(wanted);
+            wanted.Load(demon);
+        }
+    }
 }
--- Scripts/GUI/Tables/WantedTable.cs
+++ Scripts/GUI/Tables/WantedTable.cs
@@ -2,26 +2,58 @@ using System;
 using Godot;
 
 public class WantedTable : MarginContainer {
+    public static WantedTable Hovered = null;
+    public static WantedTable Instance () {
+        return (WantedTable) ResourceLoader.Load<PackedScene>("res://Nodes/GUI/Tables/WantedTable.tscn").Instance();
+    }
     [Export] NodePath namePath;
     [Export] NodePath difficultyPath;
     [Export] NodePath weaknessPath;
     [Export] NodePath rewardPath;
+    [Export] NodePath huntPath;
     Label nameField;
     Label difficultyField;
     Label weaknessField;
     Label rewardField;
-    public override void _Ready() {
+    Label huntField;
+
+    Demon demon;
+    public override void _Ready () {
         nameField = GetNode<Label>(namePath);
         difficultyField = GetNode<Label>(difficultyPath);
         weaknessField = GetNode<Label>(weaknessPath);
         rewardField = GetNode<Label>(rewardPath);
-        Load(new Demon());
+        huntField = GetNode<Label>(huntPath);
+        Connect("mouse_entered", this, nameof(MouseEntered));
+        Connect("mouse_exited", this, nameof(MouseExited));
+        Connect("focus_entered", this, nameof(FocusEntered));
+        Connect("focus_exited", this, nameof(FocusExited));
     }
 
-    public void Load(Demon demon) {
+    public void Load (Demon _demon) {
+        demon = _demon;
         nameField.Text = demon.Name;
         difficultyField.Text = demon.Difficulty;
         weaknessField.Text = demon.Weaknesses;
         rewardField.Text = $"{demon.Reward} mon";
+    }
+
+    public override void _Input (InputEvent @event) {
+        if (GetFocusOwner() == this && @event is InputEventMouseButton eventMouse && eventMouse.ButtonIndex == (int) ButtonList.Left && eventMouse.Pressed) {
+            GD.Print($"TODO: Load demon {demon?.Name}");
+        }
+    }
+
+    private void MouseEntered () {
+        GrabFocus();
+    }
+    private void MouseExited () {
+        ReleaseFocus();
+    }
+    private void FocusEntered () {
+        huntField.AddColorOverride("font_color", Colors.Red);
+    }
+    private void FocusExited () {
+        huntField.AddColorOverride("font_color", Colors.Black);
     }
 }
