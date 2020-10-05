c26c98e095c1ddce164a820c5d90ffb45fc1e480
Hedi <hedi@sellami.dev>
2020-10-06 01:49:46 +0900

    Image on wanted poster

--- Scripts/GUI/Tables/WantedTable.cs
+++ Scripts/GUI/Tables/WantedTable.cs
@@ -7,12 +7,14 @@ public class WantedTable : MarginContainer {
     public static WantedTable Instance () {
         return (WantedTable) ResourceLoader.Load<PackedScene>("res://Nodes/GUI/Tables/WantedTable.tscn").Instance();
     }
+
     [Export(PropertyHint.File)] string combatScenePath;
     [Export] NodePath namePath;
     [Export] NodePath difficultyPath;
     [Export] NodePath weaknessPath;
     [Export] NodePath rewardPath;
     [Export] NodePath huntPath;
+    [Export] NodePath imagePath;
     Label nameField;
     Label difficultyField;
     Label weaknessField;
@@ -37,6 +39,7 @@ public class WantedTable : MarginContainer {
         nameField.Text = demon.Name;
         difficultyField.Text = demon.Difficulty;
         weaknessField.Text = demon.Weaknesses;
+        GetNode<TextureRect>(imagePath).Texture = CardTextures.Instance.GetNode<Node2D>("Demons").GetNode<Sprite>(demon.Name).Texture;
         rewardField.BbcodeText = $"[right]{demon.Reward} {BB.Mon}[/right]  ";
     }
 
